ce4acd5fdb4051ea9619352c22231a28a9e89561
Nota Name <email@thisisnotanemail.com>
2023-10-09 12:06:39 -0700

    whoops

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
-document.getElementById("spinner").style.display = "block";
-
-async function discoverStories() {
-    // Dummy function that would ideally fetch the list of story folders
-    return ['default', 'catonabeach', 'example1'];
+async function isPlaceholderStory(folder) {
+    const response = await fetch(`stories/morestories/${folder}/story.json`);
+    if (!response.ok) return true;
+    const storyData = await response.json();
+    return storyData.placeholder === true;
 }
 
-async function loadStory(storyName) {
-    const response = await fetch(`stories/${storyName}/story.json`);
-    const gameData = await response.json();
-
-    function getRandomImage(pageNumber) {
-      const images = [
-        `stories/${storyName}/page${pageNumber}-a.jpg`,
-        `stories/${storyName}/page${pageNumber}-b.jpg`,
-        `stories/${storyName}/page${pageNumber}-c.jpg`
-      ];
-      const randomIndex = Math.floor(Math.random() * images.length);
-      return images[randomIndex];
+async function discoverStories() {
+    const storyFolders = Array.from({length: 99}, (_, i) => String(i + 1).padStart(2, '0'));
+    const storyList = [];
+    for (const folder of storyFolders) {
+        const isPlaceholder = await isPlaceholderStory(folder);
+        if (!isPlaceholder) storyList.push(folder);
     }
-
-    function updatePage(pageNumber) {
-      const pageData = gameData.pages[pageNumber];
-      document.getElementById('game-image').src = getRandomImage(pageNumber);
-      document.getElementById('narrative-text').innerHTML = pageData.text;
-      document.getElementById('optionA').innerText = pageData.options[0].text;
-      document.getElementById('optionB').innerText = pageData.options[1].text;
-      document.getElementById('optionA').onclick = () => updatePage(pageData.options[0].nextPage);
-      document.getElementById('optionB').onclick = () => updatePage(pageData.options[1].nextPage);
-    }
-
-    updatePage("1");
+    return storyList;
 }
 
 async function populateStories() {
-    try {
-        const stories = await discoverStories();
-        const selectElement = document.getElementById('story-select');
-        stories.forEach(story => {
-            const option = document.createElement('option');
-            option.text = story;
-            option.value = story;
-            selectElement.appendChild(option);
-        });
-        if (stories.length > 0) {
-            const randomIndex = Math.floor(Math.random() * stories.length);
-            loadStory(stories[randomIndex]);
-        }
-    } catch (error) {
-        console.error(error);
-    } finally {
-        document.getElementById("spinner").style.display = "none";
+    const stories = await discoverStories();
+    const selectElement = document.getElementById("story-select");
+    stories.forEach(story => {
+        const option = document.createElement('option');
+        option.text = story;
+        option.value = story;
+        selectElement.appendChild(option);
+    });
+    if (stories.length > 0) loadStory(stories[0]);
+}
+
+async function loadStory(storyName) {
+    const response = await fetch(`stories/morestories/${storyName}/story.json`);
+    const gameData = await response.json();
+
+    function getRandomImage(pageNumber) {
+        const images = [
+            `stories/morestories/${storyName}/page${pageNumber}-a.jpg`,
+            `stories/morestories/${storyName}/page${pageNumber}-b.jpg`,
+            `stories/morestories/${storyName}/page${pageNumber}-c.jpg`
+        ];
+        const randomIndex = Math.floor(Math.random() * images.length);
+        return images[randomIndex];
     }
+
+    function updatePage(pageNumber) {
+        const pageData = gameData.pages[pageNumber];
+        document.getElementById('game-image').src = getRandomImage(pageNumber);
+        document.getElementById('narrative-text').innerHTML = pageData.text;
+        document.getElementById('optionA').innerText = pageData.options[0].text;
+        document.getElementById('optionB').innerText = pageData.options[1].text;
+        document.getElementById('optionA').onclick = () => updatePage(pageData.options[0].nextPage);
+        document.getElementById('optionB').onclick = () => updatePage(pageData.options[1].nextPage);
+    }
+
+    updatePage("1");
 }
 
 document.getElementById('mode-toggle').addEventListener('click', function() {
@@ -68,12 +69,12 @@ document.getElementById('story-select').addEventListener('change', function() {
 populateStories();
 
 document.getElementById('fab-button').addEventListener('click', function() {
-  const controls = document.getElementById('fab-controls');
-  controls.classList.toggle('hidden');
+    const controls = document.getElementById('fab-controls');
+    controls.classList.toggle('hidden');
 });
 
 document.querySelectorAll('.fab-controls button, .fab-controls select').forEach(el => {
-  el.addEventListener('click', function() {
-    document.getElementById('fab-controls').classList.add('hidden');
-  });
+    el.addEventListener('click', function() {
+        document.getElementById('fab-controls').classList.add('hidden');
+    });
 });
