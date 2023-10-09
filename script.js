a2533c4c8e6480b4083ed23f268c84408e3f4693
Nota Name <email@thisisnotanemail.com>
2023-10-09 14:54:06 -0700

    images

--- script.js
+++ script.js
@@ -1,72 +1,75 @@
 const md = new markdownit();
 
+function setRandomSplashImage() {
+  const randomNumber = Math.floor(Math.random() * 4) + 1;
+  document.getElementById('splash-image').src = `splash${randomNumber}.jpg`;
+}
+
 async function isPlaceholderStory(folder) {
-    const response = await fetch(`stories/morestories/${folder}/story.json`);
-    if (!response.ok) return true;
-    const storyData = await response.json();
-    return storyData.placeholder === true;
+  const response = await fetch(`stories/morestories/${folder}/story.json`);
+  if (!response.ok) return true;
+  const storyData = await response.json();
+  return storyData.placeholder === true;
 }
 
 async function discoverStories() {
-    const storyFolders = Array.from({length: 99}, (_, i) => String(i + 1).padStart(2, '0'));
-    const storyList = [];
-    for (const folder of storyFolders) {
-        const isPlaceholder = await isPlaceholderStory(folder);
-        if (!isPlaceholder) storyList.push(folder);
-    }
-    return storyList;
+  const storyFolders = Array.from({length: 99}, (_, i) => String(i + 1).padStart(2, '0'));
+  const storyList = [];
+  for (const folder of storyFolders) {
+    const isPlaceholder = await isPlaceholderStory(folder);
+    if (!isPlaceholder) storyList.push(folder);
+  }
+  return storyList;
 }
 
 async function populateStories() {
-    document.getElementById('spinner').classList.remove('d-none');
-    const stories = await discoverStories();
-    const selectElement = document.getElementById("story-select");
-    stories.forEach(story => {
-        const option = document.createElement('option');
-        option.text = story;
-        option.value = story;
-        selectElement.appendChild(option);
-    });
-    if (stories.length > 0) loadStory(stories[Math.floor(Math.random() * stories.length)]);
-    document.getElementById('spinner').classList.add('d-none');
+  document.getElementById('spinner').classList.remove('d-none');
+  const stories = await discoverStories();
+  const selectElement = document.getElementById("story-select");
+  stories.forEach(story => {
+    const option = document.createElement('option');
+    option.text = story;
+    option.value = story;
+    selectElement.appendChild(option);
+  });
+  if (stories.length > 0) loadStory(stories[Math.floor(Math.random() * stories.length)]);
+  document.getElementById('spinner').classList.add('d-none');
 }
 
 async function loadStory(storyName) {
-    const response = await fetch(`stories/morestories/${storyName}/story.json`);
-    const gameData = await response.json();
+  const response = await fetch(`stories/morestories/${storyName}/story.json`);
+  const gameData = await response.json();
 
-    // Set splash image when story is loaded
-    document.getElementById('splash-image').src = `stories/morestories/${storyName}/splash.jpg`;
+  function updatePage(pageNumber) {
+    const pageData = gameData.pages[pageNumber];
+    document.getElementById('game-image').src = `stories/morestories/${storyName}/page${pageNumber}.jpg`;
+    document.getElementById('narrative-text').innerHTML = md.render(pageData.text);
+    document.getElementById('optionA').innerText = pageData.options[0].text;
+    document.getElementById('optionB').innerText = pageData.options[1].text;
+    document.getElementById('optionA').onclick = () => updatePage(pageData.options[0].nextPage);
+    document.getElementById('optionB').onclick = () => updatePage(pageData.options[1].nextPage);
+  }
 
-    function updatePage(pageNumber) {
-        const pageData = gameData.pages[pageNumber];
+  document.getElementById('splash-image').classList.add('d-none');
+  document.getElementById('game-image').classList.remove('d-none');
 
-        // Update game image based on current page number
-        document.getElementById('game-image').src = `stories/morestories/${storyName}/page${pageNumber}.jpg`;
-
-        document.getElementById('narrative-text').innerHTML = md.render(pageData.text);
-        document.getElementById('optionA').innerText = pageData.options[0].text;
-        document.getElementById('optionB').innerText = pageData.options[1].text;
-        document.getElementById('optionA').onclick = () => updatePage(pageData.options[0].nextPage);
-        document.getElementById('optionB').onclick = () => updatePage(pageData.options[1].nextPage);
-    }
-
-    updatePage("1");
+  updatePage("1");
 }
 
 document.getElementById('mode-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    document.getElementById('fab-controls').classList.add('d-none');
+  document.body.classList.toggle('dark-mode');
+  document.getElementById('fab-controls').classList.add('d-none');
 });
 
 document.getElementById('story-select').addEventListener('change', function() {
-    loadStory(this.value);
-    document.getElementById('fab-controls').classList.add('d-none');
+  loadStory(this.value);
+  document.getElementById('fab-controls').classList.add('d-none');
 });
 
+setRandomSplashImage();
 populateStories();
 
 document.getElementById('fab-button').addEventListener('click', function() {
-    const controls = document.getElementById('fab-controls');
-    controls.classList.toggle('d-none');
+  const controls = document.getElementById('fab-controls');
+  controls.classList.toggle('d-none');
 });
