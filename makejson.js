8c55b2fad352d72f124185f5beabf3fc66e3fce2
Nota Name <email@thisisnotanemail.com>
2023-10-08 22:00:45 -0700

    mj makeup

--- makejson.js
+++ makejson.js
@@ -7,78 +7,86 @@ function addPage(i, data = {}) {
         <div class="card-title h5">Page ${i}</div>
       </div>
       <div class="card-body">
-        <label>Narrative Text: <textarea name="text-${i}" class="textarea">${data.text || ''}</textarea></label><br>
-        <label>Setting: <input type="text" name="setting-${i}" class="input" value="${data.metadata?.Setting || ''}"></label><br>
-        <label>Time: <input type="text" name="time-${i}" class="input" value="${data.metadata?.Time || ''}"></label><br>
-        <label>Option A Text: <textarea name="optionA-${i}" class="textarea">${data.options?.[0]?.text || ''}</textarea></label><br>
-        <label>Option A Next Page: <input type="text" name="optionA-next-${i}" class="input" value="${data.options?.[0]?.nextPage || ''}"></label><br>
-        <label>Option B Text: <textarea name="optionB-${i}" class="textarea">${data.options?.[1]?.text || ''}</textarea></label><br>
-        <label>Option B Next Page: <input type="text" name="optionB-next-${i}" class="input" value="${data.options?.[1]?.nextPage || ''}"></label><br>
+        <label>Narrative Text: <textarea name="text-${i}" class="narrative-text">${data.text || ''}</textarea></label><br>
+        <label>Setting: <input type="text" name="setting-${i}" value="${data.metadata?.Setting || ''}"></label><br>
+        <label>Time: <input type="text" name="time-${i}" value="${data.metadata?.Time || ''}"></label><br>
+        <label>Option A Text: <textarea name="optionA-${i}" class="option-text">${data.options?.[0]?.text || ''}</textarea></label><br>
+        <label>Option A Next Page: <input type="text" name="optionA-next-${i}" value="${data.options?.[0]?.nextPage || ''}"></label><br>
+        <label>Option B Text: <textarea name="optionB-${i}" class="option-text">${data.options?.[1]?.text || ''}</textarea></label><br>
+        <label>Option B Next Page: <input type="text" name="optionB-next-${i}" value="${data.options?.[1]?.nextPage || ''}"></label><br>
       </div>
     `;
     document.getElementById('pages').appendChild(newPage);
   }
   
-  addPage(1);
+  document.addEventListener('DOMContentLoaded', function () {
+    addPage(1);
   
-  document.getElementById('add-page').addEventListener('click', function() {
-    const nextPage = document.querySelectorAll('.page').length + 1;
-    addPage(nextPage);
-  });
-  
-  document.getElementById('generate-json').addEventListener('click', function() {
-    const jsonData = { pages: {} };
-    for (const pageDiv of document.querySelectorAll('.page')) {
-      const i = pageDiv.id.split('-')[1];
-      const form = document.forms['json-form'];
-      jsonData.pages[i] = {
-        text: form[`text-${i}`].value,
-        metadata: {
-          Setting: form[`setting-${i}`].value,
-          Time: form[`time-${i}`].value
-        },
-        options: [
-          {
-            text: form[`optionA-${i}`].value,
-            nextPage: form[`optionA-next-${i}`].value
-          },
-          {
-            text: form[`optionB-${i}`].value,
-            nextPage: form[`optionB-next-${i}`].value
-          }
-        ]
-      };
-    }
-    document.getElementById('json-output').textContent = JSON.stringify(jsonData, null, 2);
-  });
-  
-  document.getElementById('import-button').addEventListener('click', function() {
-    const fileInput = document.getElementById('import-json');
-    const file = fileInput.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-        const jsonData = JSON.parse(e.target.result);
-        document.getElementById('pages').innerHTML = '';
-        for (const [i, data] of Object.entries(jsonData.pages)) {
-          addPage(i, data);
-        }
-      };
-      reader.readAsText(file);
-    }
-  });
-  
-  document.getElementById('copy-json').addEventListener('click', function() {
-    const jsonText = document.getElementById('json-output').textContent;
-    navigator.clipboard.writeText(jsonText).then(function() {
-      alert('JSON copied to clipboard');
-    }).catch(function(err) {
-      alert('Could not copy JSON: ', err);
+    document.getElementById('add-page').addEventListener('click', function () {
+      const nextPage = document.querySelectorAll('.page').length + 1;
+      addPage(nextPage);
     });
-  });
   
-  document.getElementById('info-button').addEventListener('click', function() {
-    const infoModal = document.getElementById('info-modal');
-    infoModal.classList.add('active');
+    document.getElementById('generate-json').addEventListener('click', function () {
+      const jsonData = {
+        pages: {}
+      };
+  
+      for (const pageDiv of document.querySelectorAll('.page')) {
+        const i = pageDiv.id.split('-')[1];
+        const form = document.forms['json-form'];
+        jsonData.pages[i] = {
+          text: form[`text-${i}`].value,
+          metadata: {
+            Setting: form[`setting-${i}`].value,
+            Time: form[`time-${i}`].value
+          },
+          options: [
+            {
+              text: form[`optionA-${i}`].value,
+              nextPage: form[`optionA-next-${i}`].value
+            },
+            {
+              text: form[`optionB-${i}`].value,
+              nextPage: form[`optionB-next-${i}`].value
+            }
+          ]
+        };
+      }
+      document.getElementById('json-output').textContent = JSON.stringify(jsonData, null, 2);
+    });
+  
+    document.getElementById('import-button').addEventListener('click', function () {
+      const fileInput = document.getElementById('import-json');
+      const file = fileInput.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const jsonData = JSON.parse(e.target.result);
+          document.getElementById('pages').innerHTML = '';
+          for (const [i, data] of Object.entries(jsonData.pages)) {
+            addPage(i, data);
+          }
+        };
+        reader.readAsText(file);
+      }
+    });
+  
+    document.getElementById('copy-json').addEventListener('click', function () {
+      const jsonText = document.getElementById('json-output').textContent;
+      navigator.clipboard.writeText(jsonText).then(function () {
+        alert('JSON copied to clipboard');
+      }).catch(function (err) {
+        alert('Could not copy JSON: ', err);
+      });
+    });
+  
+    document.getElementById('info-button').addEventListener('click', function () {
+      document.getElementById('info-modal').classList.add('active');
+    });
+  
+    document.getElementById('close-info').addEventListener('click', function () {
+      document.getElementById('info-modal').classList.remove('active');
+    });
   });
   
