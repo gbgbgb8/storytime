a18a2c990bad60dcf5e5760b196f08821c13ac93
Nota Name <email@thisisnotanemail.com>
2023-10-09 09:34:06 -0700

    js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-// Function to populate story dropdown dynamically
+// Populate story dropdown dynamically
 async function populateStories() {
-    // Assuming an API or function that fetches the available story names in the 'stories' folder
-    const stories = ['default', 'catonabeach', 'example1']; // Placeholder, replace with actual API call
+    const stories = ['default', 'catonabeach', 'example1'];
     const selectElement = document.getElementById('story-select');
-  
     stories.forEach(story => {
       const option = document.createElement('option');
       option.text = story;
       option.value = story;
       selectElement.appendChild(option);
     });
-    
-    // Initialize game with the first story
     loadStory(stories[0]);
   }
   
-  // Function to load a selected story
+  // Load a selected story
   async function loadStory(storyName) {
     const response = await fetch(`stories/${storyName}/story.json`);
     const gameData = await response.json();
@@ -33,7 +29,7 @@ async function populateStories() {
     function updatePage(pageNumber) {
       const pageData = gameData.pages[pageNumber];
       document.getElementById('game-image').src = getRandomImage(pageNumber);
-      document.getElementById('narrative-text').querySelector('.card-body').innerHTML = pageData.text;
+      document.getElementById('narrative-text').innerHTML = pageData.text;
       document.getElementById('optionA').innerText = pageData.options[0].text;
       document.getElementById('optionB').innerText = pageData.options[1].text;
       document.getElementById('optionA').onclick = () => updatePage(pageData.options[0].nextPage);
@@ -43,12 +39,14 @@ async function populateStories() {
     updatePage("1");
   }
   
+  // Toggle dark mode
   document.getElementById('mode-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('bg-dark');
   });
   
-  if (!document.body.classList.contains('dark-mode')) {
-    document.body.classList.add('dark-mode');
+  // Initialize dark mode if not set
+  if (!document.body.classList.contains('bg-dark')) {
+    document.body.classList.add('bg-dark');
   }
   
   // Listen for story selection change
