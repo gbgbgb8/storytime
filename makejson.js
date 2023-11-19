3d80687c501cc1a89393c5c6911209432a104516
Nota Name <email@thisisnotanemail.com>
2023-11-19 12:28:19 -0800

    visualization maybe

--- makejson.js
+++ makejson.js
@@ -1,34 +1,5 @@
-function addPage(i, data = {}) {
-    const newPage = document.createElement('div');
-    newPage.className = 'page card bg-dark text-light mb-3';
-    newPage.id = `page-${i}`;
-    newPage.innerHTML = `
-      <div class="card-header">
-        <h5 class="card-title">Page ${i}</h5>
-      </div>
-      <div class="card-body">
-        <label for="text-${i}" class="form-label">Narrative Text:</label>
-        <textarea id="text-${i}" name="text-${i}" class="form-control bg-secondary text-light" rows="5">${data.text || ''}</textarea>
-        <label for="setting-${i}" class="form-label mt-2">Setting:</label>
-        <input type="text" id="setting-${i}" name="setting-${i}" class="form-control bg-secondary text-light" value="${data.metadata?.Setting || ''}">
-        <label for="time-${i}" class="form-label mt-2">Time:</label>
-        <input type="text" id="time-${i}" name="time-${i}" class="form-control bg-secondary text-light" value="${data.metadata?.Time || ''}">
-        <label for="optionA-${i}" class="form-label mt-2">Option A Text:</label>
-        <textarea id="optionA-${i}" name="optionA-${i}" class="form-control bg-secondary text-light" rows="3">${data.options?.[0]?.text || ''}</textarea>
-        <label for="optionA-next-${i}" class="form-label mt-2">Option A Next Page:</label>
-        <input type="text" id="optionA-next-${i}" name="optionA-next-${i}" class="form-control bg-secondary text-light" value="${data.options?.[0]?.nextPage || ''}">
-        <label for="optionB-${i}" class="form-label mt-2">Option B Text:</label>
-        <textarea id="optionB-${i}" name="optionB-${i}" class="form-control bg-secondary text-light" rows="3">${data.options?.[1]?.text || ''}</textarea>
-        <label for="optionB-next-${i}" class="form-label mt-2">Option B Next Page:</label>
-        <input type="text" id="optionB-next-${i}" name="optionB-next-${i}" class="form-control bg-secondary text-light" value="${data.options?.[1]?.nextPage || ''}">
-      </div>
-    `;
-    document.getElementById('pages').appendChild(newPage);
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     addPage(1);
-    const jsonForm = document.forms['json-form'];
 
     document.getElementById('add-page').addEventListener('click', function () {
         const nextPage = document.querySelectorAll('.page').length + 1;
@@ -36,32 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.getElementById('generate-json').addEventListener('click', function () {
-        const jsonData = {
-            title: document.getElementById('story-title').value,
-            author: document.getElementById('story-author').value,
-            comments: document.getElementById('story-comments').value,
-            pages: {}
-        };
-        document.querySelectorAll('.page').forEach(pageDiv => {
-            const i = pageDiv.id.split('-')[1];
-            jsonData.pages[i] = {
-                text: jsonForm[`text-${i}`].value,
-                metadata: {
-                    Setting: jsonForm[`setting-${i}`].value,
-                    Time: jsonForm[`time-${i}`].value
-                },
-                options: [
-                    {
-                        text: jsonForm[`optionA-${i}`].value,
-                        nextPage: jsonForm[`optionA-next-${i}`].value
-                    },
-                    {
-                        text: jsonForm[`optionB-${i}`].value,
-                        nextPage: jsonForm[`optionB-next-${i}`].value
-                    }
-                ]
-            };
-        });
+        const jsonData = generateJsonData();
         document.getElementById('json-output').textContent = JSON.stringify(jsonData, null, 2);
     });
 
@@ -94,9 +40,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById('story-author').value = jsonData.author;
                 document.getElementById('story-comments').value = jsonData.comments;
                 document.getElementById('pages').innerHTML = '';
-                for (const [i, data] of Object.entries(jsonData.pages)) {
+                Object.entries(jsonData.pages).forEach(([i, data]) => {
                     addPage(i, data);
-                }
+                });
             };
             reader.readAsText(file);
         }
@@ -110,4 +56,91 @@ document.addEventListener('DOMContentLoaded', function () {
             alert('Could not copy JSON: ', err);
         });
     });
+
+    document.getElementById('flowchart-button').addEventListener('click', function () {
+        const jsonData = generateJsonData();
+        const { nodes, edges } = createFlowchartData(jsonData);
+        displayFlowchart(nodes, edges);
+    });
 });
+
+function addPage(i, data = {}) {
+    const newPage = document.createElement('div');
+    newPage.className = 'page card bg-dark text-light mb-3';
+    newPage.id = `page-${i}`;
+    newPage.innerHTML = `
+        <div class="card-header">
+            <h5 class="card-title">Page ${i}</h5>
+        </div>
+        <div class="card-body">
+            <label for="text-${i}" class="form-label">Narrative Text:</label>
+            <textarea id="text-${i}" name="text-${i}" class="form-control bg-secondary text-light" rows="5">${data.text || ''}</textarea>
+            <label for="setting-${i}" class="form-label mt-2">Setting:</label>
+            <input type="text" id="setting-${i}" name="setting-${i}" class="form-control bg-secondary text-light" value="${data.metadata?.Setting || ''}">
+            <label for="time-${i}" class="form-label mt-2">Time:</label>
+            <input type="text" id="time-${i}" name="time-${i}" class="form-control bg-secondary text-light" value="${data.metadata?.Time || ''}">
+            <label for="optionA-${i}" class="form-label mt-2">Option A Text:</label>
+            <textarea id="optionA-${i}" name="optionA-${i}" class="form-control bg-secondary text-light" rows="3">${data.options?.[0]?.text || ''}</textarea>
+            <label for="optionA-next-${i}" class="form-label mt-2">Option A Next Page:</label>
+            <input type="text" id="optionA-next-${i}" name="optionA-next-${i}" class="form-control bg-secondary text-light" value="${data.options?.[0]?.nextPage || ''}">
+            <label for="optionB-${i}" class="form-label mt-2">Option B Text:</label>
+            <textarea id="optionB-${i}" name="optionB-${i}" class="form-control bg-secondary text-light" rows="3">${data.options?.[1]?.text || ''}</textarea>
+            <label for="optionB-next-${i}" class="form-label mt-2">Option B Next Page:</label>
+            <input type="text" id="optionB-next-${i}" name="optionB-next-${i}" class="form-control bg-secondary text-light" value="${data.options?.[1]?.nextPage || ''}">
+        </div>
+    `;
+    document.getElementById('pages').appendChild(newPage);
+}
+
+function generateJsonData() {
+    const jsonData = {
+        title: document.getElementById('story-title').value,
+        author: document.getElementById('story-author').value,
+        comments: document.getElementById('story-comments').value,
+        pages: {}
+    };
+    document.querySelectorAll('.page').forEach(pageDiv => {
+        const i = pageDiv.id.split('-')[1];
+        jsonData.pages[i] = {
+            text: document.forms['json-form'][`text-${i}`].value,
+            metadata: {
+                Setting: document.forms['json-form'][`setting-${i}`].value,
+                Time: document.forms['json-form'][`time-${i}`].value
+            },
+            options: [
+                {
+                    text: document.forms['json-form'][`optionA-${i}`].value,
+                    nextPage: document.forms['json-form'][`optionA-next-${i}`].value
+                },
+                {
+                    text: document.forms['json-form'][`optionB-${i}`].value,
+                    nextPage: document.forms['json-form'][`optionB-next-${i}`].value
+                }
+            ]
+        };
+    });
+    return jsonData;
+}
+
+function createFlowchartData(jsonData) {
+    const nodes = [];
+    const edges = [];
+
+    Object.entries(jsonData.pages).forEach(([pageId, pageData]) => {
+        nodes.push({ id: pageId, label: `Page ${pageId}` });
+        pageData.options.forEach((option, index) => {
+            if (option.nextPage) {
+                edges.push({ from: pageId, to: option.nextPage, label: `Option ${index + 1}: ${option.text}` });
+            }
+        });
+    });
+
+    return { nodes: new vis.DataSet(nodes), edges: new vis.DataSet(edges) };
+}
+
+function displayFlowchart(nodes, edges) {
+    const container = document.getElementById('flowchart');
+    const data = { nodes, edges };
+    const options = {}; // Define your vis.js options here
+    new vis.Network(container, data, options);
+}
